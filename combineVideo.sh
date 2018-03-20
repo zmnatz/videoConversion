47b1f9dbb5d47a4aeaa6a3756efb2f1d387c984f
Zaven Mnatzakanian <you@example.com>
2018-03-20 05:55:11 -0700

    Updated combiner scripts to take folders as parameters

--- combineVideo.sh
+++ combineVideo.sh
@@ -1,14 +1,7 @@
-for i in *;do
-cd "$i"
-find . -name '*.MP4' -exec echo "file {}" \; > inputs.txt
-
-name=$i
-# name=${PWD##*/}
-
-echo "$name"
-
-ffmpeg -f concat -i inputs.txt -c copy "$name.mp4"
-ffmpeg -i "$name.mp4" -acodec copy -vcodec copy "../$name.mkv"
-rm "$name.mp4"
-rm inputs.txt
+for arg
+do printf "file '%s'\n" ./"$arg"/*.MP4 > "$arg.txt" 
+ffmpeg -f concat -i "$arg.txt" -c copy "$arg.mp4"
+ffmpeg -i "$arg.mp4" -acodec copy -vcodec copy "$arg.mkv"
+rm "$arg.mp4"
+rm "$arg.txt"
 done
